7a0ee9507ef68acfb9386e831b7e09edccaec0a2
kumar9323 <venkatsuneel3@gmail.com>
2026-04-14 20:03:55 +0530

    inital commit

---
+++ test_calculator.py
@@ -0,0 +1,16 @@
+from calculator import *
+
+def test_add():
+    assert add(10, 5) == 15
+
+def test_sub():
+    assert sub(10, 5) == 5
+
+def test_mul():
+    assert mul(10, 5) == 50
+
+def test_div():
+    assert div(10, 5) == 2
+
+def test_div_by_zero():
+    assert div(10, 0) == "Error: Division by zero"
