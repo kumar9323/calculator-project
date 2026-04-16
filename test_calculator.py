d4698e02d0b606a6a177779aab4826400e645841
kumar9323 <venkatsuneel3@gmail.com>
2026-04-16 07:30:54 +0530

    new test cases are added

--- test_calculator.py
+++ test_calculator.py
@@ -3,8 +3,14 @@ from calculator import *
 def test_add():
     assert add(10, 5) == 15
 
+def test_addition():
+    assert add(1,2) == 3
+
 def test_sub():
     assert sub(10, 5) == 5
+
+def test_subraction():
+    assert sub(9,5) == 4
 
 def test_mul():
     assert mul(10, 5) == 50
